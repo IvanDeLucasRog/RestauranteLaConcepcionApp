e996ba8bcc7713afb89f99d8bbd0df95b6887164
Iván de Lucas Rogero <ivandelucasdeveloper@gmail.com>
2020-06-19 17:12:25 +0200

    Mapa terminado

--- app/src/main/java/com/platzi/conf/model/Ubication.kt
+++ app/src/main/java/com/platzi/conf/model/Ubication.kt
@@ -1,11 +1,11 @@
 package com.platzi.conf.model
 
 class Ubication {
-    val name = ""
-    val address = ""
-    val latitude = ""
-    val longitude= ""
-    val phone = ""
-    val website = ""
-    val photo = ""
+    val name = "Restaurante La Concepción"
+    val address = "Plaza Mayor, 15, 40001, Segovia"
+    val latitude = 40.950358
+    val longitude= -4.124385
+    val phone = "+34 921 46 09 25"
+    val website = "https://www.restaurantelaconcepcion.com/"
+    val photo = "https://www.restaurantelaconcepcion.com/wp-content/uploads/2019/05/IMG_0281-1024x768.jpg"
 }
--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,18 +1,30 @@
 package com.platzi.conf.view.ui.fragments
 
 
+import android.graphics.Bitmap
+import android.graphics.Camera
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 
 import com.platzi.conf.R
+import com.platzi.conf.model.Ubication
+import com.google.type.LatLng as LatLng
 
 /**
  * A simple [Fragment] subclass.
  */
-class UbicationFragment : Fragment() {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +34,36 @@ class UbicationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+        val zoom = 16f
+        val centerMap = com.google.android.gms.maps.model.LatLng(ubication.latitude, ubication.longitude)
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = com.google.android.gms.maps.model.LatLng(ubication.latitude, ubication.longitude)
+        val markerOption = MarkerOptions()
+        markerOption.position(centerMap)
+        markerOption.title("Restaurante La Concepcion")
+
+        val bitmapdraw = context?.applicationContext?.let {ContextCompat.getDrawable(it, R.drawable.ic_restaurantpin)} as BitmapDrawable
+        val smallMarker = Bitmap.createScaledBitmap(bitmapdraw.bitmap, 150, 150, false)
+        markerOption.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+        googleMap?.addMarker(markerOption)
+        googleMap?.setOnMarkerClickListener (this)
+
+        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.custom_map))
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragmentDialog)
+        return true
+    }
 
 }
