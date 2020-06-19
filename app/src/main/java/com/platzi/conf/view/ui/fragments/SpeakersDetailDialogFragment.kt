389537d8a58ff7f29d66a65c32362dd33421aed5
Iván de Lucas Rogero <ivandelucasdeveloper@gmail.com>
2020-06-19 16:09:09 +0200

    Terminado el speakers Detail Dialog Fragment a falta de las pruebas

--- app/src/main/java/com/platzi/conf/view/ui/fragments/SpeakersDetailDialogFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/SpeakersDetailDialogFragment.kt
@@ -1,18 +1,28 @@
 package com.platzi.conf.view.ui.fragments
 
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
 
 import com.platzi.conf.R
+import com.platzi.conf.model.Speaker
+import kotlinx.android.synthetic.main.fragment_speakers_detail_dialog.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class SpeakersDetailDialogFragment : Fragment() {
+class SpeakersDetailDialogFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +30,25 @@ class SpeakersDetailDialogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_speakers_detail_dialog, container, false)
+        toolbarSpeaker.navigationIcon = ContextCompat.getDrawable(view!!.context, R.drawable.ic_close)
+        toolbarSpeaker.setTitleTextColor(Color.WHITE)
+        toolbarSpeaker.setNavigationOnClickListener {
+            dismiss()
+        }
+        val speaker = arguments?.getSerializable("speaker") as Speaker
+        toolbarSpeaker.title = speaker.name
+        tvDetailSpeakerName.text = speaker.name
+
+        tvDetailSpeakerJobtitle.text = speaker.jobtitle
+        tvDetailSpeakerWorkplace.text = speaker.workplace
+        //ivDetailSpeakerTwitter
+        tvDetailSpeakerBiography.text = speaker.biography
     }
 
-
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+    }
 }
+
+
